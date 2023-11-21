bb59869ab4ce0106c6a52f8ac9e0013a7e3a286d
Luis Miguel Lima Tavares <up202108662@fe.up.pt>
2023-11-21 17:27:04 +0000

    small fix to post.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Post extends Model
 {
     use HasFactory;
@@ -24,4 +25,4 @@ class Post extends Model
    {
       return $this->belongsTo(User::class, 'owner_id', 'user_id');
    }
-
+}
