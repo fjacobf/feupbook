4d186aa3d9369d30c055eb6e02f423f80afd9b9f
RodrigoCamposRodrigues <up202108847@fe.up.pt>
2023-12-04 11:06:51 +0000

    feat: :sparkles: Comments showing on post page. Post relative time.

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -6,6 +6,53 @@
 
 @section('content')
     <section class="d-flex justify-content-center" style="margin-left: 280px;">
-        @include('partials.post', ['post' => $post])
+        <div class="post mt-4">
+            <div class="card" style="width: 50em;">
+                <div class="card-header d-flex justify-content-between fs-5">
+                    <small><a href="{{ route('user.profile', ['id' => $post->user->user_id]) }}" class="link-primary:hover">{{$post->user->name}}</a></small>
+                    <small class="text-muted"><span class="text-muted">@</span>{{ $post->user->username }}</small>
+                    <small class="text-black">{{ \Carbon\Carbon::parse($post->created_at)->format('H:i d-m-y') }}</small>
+                </div>
+
+                <div class="card-body">
+                    <h5 class="card-text text-black">{{ $post->content }}</h5>
+                </div>
+
+                <div class="card-footer d-flex justify-content-around">
+                    <button class="btn bi bi-heart custom-btn-like"></button>
+                    <a href="{{ route('showPost', ['id' => $post->post_id]) }}" class="btn bi bi-chat custom-btn-comment"></a>
+                    <button class="btn bi bi-bookmark custom-btn-bookmark"></button>
+
+                    @canany(['update', 'delete'], $post)
+                        <div class="ms-auto">
+                                <a href="{{ route('editPost', ['id' => $post->post_id]) }}" class="btn btn-primary me-2">Edit Post</a>
+                                <form action="{{ route('deletePost', ['id' => $post->post_id]) }}" method="POST" class="d-inline">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this post?')">Delete Post</button>
+                                </form>
+                        </div>
+                    @endcanany
+                </div>
+
+                <div class="card-body">
+                    <form style="display:flex; justify-content: center" action="{{ route('storeComment') }}" method="POST">
+                        @csrf
+                        <textarea style="resize:none" id="content" name="content" cols="30" rows="1"
+                            placeholder="Adicione um comentário..."></textarea> 
+                        <input type="hidden" name="post_id" id="post_id" value="{{ $post->post_id }}">
+                        <input type="hidden" name="comment_id" id="comment_id" value="{{ NULL }}">
+                        <button type="submit" class="btn btn-primary">Comment</button>
+                    </form>
+
+                    @forelse($post->comments as $comment)
+                    @include('partials.comment', ['comment' => $comment])
+                    @empty
+                        <p style="color: gray; font-size: 0.8rem">There are no comments on this post.</p>
+                    @endforelse
+                </div>
+
+            </div>
+        </div>
     </section>
 @endsection
--- resources/views/partials/post.blade.php
+++ resources/views/partials/post.blade.php
@@ -3,7 +3,7 @@
             <div class="card-header d-flex justify-content-between fs-5">
                 <small><a href="{{ route('user.profile', ['id' => $post->user->user_id]) }}" class="link-primary:hover">{{$post->user->name}}</a></small>
                 <small class="text-muted"><span class="text-muted">@</span>{{ $post->user->username }}</small>
-                <small class="text-black">{{ \Carbon\Carbon::parse($post->created_at)->format('H:i d-m-y') }}</small>
+                <small class="text-black">{{ time_since($post->created_at) }}</small>
             </div>
             <a href="{{ route('showPost', ['id' => $post->post_id]) }}" class="text-decoration-none">
                 <div class="card-body">
@@ -29,22 +29,3 @@
             </div>
         </div>
 </div>
-
-
-<!-- <div class="card-body">
-    <h4 class="card-text">Comments:</h4>
-    @forelse($post->comments as $comment)
-    @include('partials.comment', ['comment' => $comment])
-    @empty
-        <p style="color: gray; font-size: 0.8rem">There are no comments on this post.</p>
-    @endforelse
-
-    <form style="display:flex; justify-content: center" action="{{ route('storeComment') }}" method="POST">
-        @csrf
-        <textarea style="resize:none" id="content" name="content" cols="30" rows="1"
-            placeholder="Adicione um comentário..."></textarea> 
-        <input type="hidden" name="post_id" id="post_id" value="{{ $post->post_id }}">
-        <input type="hidden" name="comment_id" id="comment_id" value="{{ NULL }}">
-        <button type="submit" class="btn btn-primary">Comment</button>
-    </form>
-</div> -->
