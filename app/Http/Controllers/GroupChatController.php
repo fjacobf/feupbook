2e6c71b4bf763d75ffa4033e3f605bcb45caff7b
Luis Miguel Lima Tavares <up202108662@fe.up.pt>
2023-12-06 17:52:10 +0000

    Fix authorization issue in sendMessage method

--- app/Http/Controllers/GroupChatController.php
+++ app/Http/Controllers/GroupChatController.php
@@ -33,19 +33,22 @@ class GroupChatController extends Controller
     }
 
     public function sendMessage(Request $request, GroupChat $groupChat)
-    {
+{
+    try {
         $this->authorize('send', $groupChat);
-
-        $message = new Message;
-        $message->content = $request->input('content');
-        $message->emitter_id = auth()->id();
-        $message->group_id = $groupChat->group_id;
-        $message->date = date('Y-m-d');
-        $message->viewed = false;
-        $message->save();
-
-        // TODO: IMPLEMENT AJAX IN MESSAGE SENT
-
-        return response()->json('Message sent');
+    } catch (AuthorizationException $e) {
+        // Redirect to a different page if the user does not have the permission to send a message
+        return redirect('/group-chats')->withErrors(['message' => 'You are not authorized to send a message to this group chat']);
     }
+
+    $message = new Message;
+    $message->content = $request->input('content');
+    $message->emitter_id = auth()->id();
+    $message->group_id = $groupChat->id;
+    $message->date = date('Y-m-d');
+    $message->viewed = false;
+    $message->save();
+
+    return response()->json('Message sent');
+}
 }
