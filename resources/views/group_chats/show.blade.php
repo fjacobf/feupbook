33c3b42640954bbec6dae7fc05ea8a701991fa9e
Luis Miguel Lima Tavares <up202108662@fe.up.pt>
2023-12-06 19:04:20 +0000

    Update group chat layout and message display

--- resources/views/group_chats/show.blade.php
+++ resources/views/group_chats/show.blade.php
@@ -7,24 +7,15 @@
 @section('content')
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-8 offset-md-4">
+            <div class="col-md-8 offset-md-2">
                 <div class="mt-4">
                     <h1>{{ $groupChat->name }}</h1>
                     <p>Description: {{ $groupChat->description }}</p>
                     <p>Owner: {{ $groupChat->owner->name }}</p>
 
                     <h2>Messages</h2>
-                    {{-- <ul>
-                        @foreach ($groupChat->messages as $message)
-                            <li>
-                                <p>Sender: {{ $message->emitter->name }}</p>
-                                <p>Date: {{ $message->date }}</p>
-                                <p>{{ $message->content }}</p>
-                            </li>
-                        @endforeach
-                    </ul> --}}
 
-                    <div id="chat"></div>
+                    <div id="chat" class="mt-3 mb-3" style="height: 400px; overflow-y: scroll; border: 1px solid #ccc; padding: 10px; border-radius: 5px;"></div>
 
                     <form method="POST" action="{{ route('group-chats.sendMessage', $groupChat->group_id) }}">
                         @csrf
@@ -39,42 +30,39 @@
         </div>
     </div>
 
-    <div id="chat"></div>
-
     <script>            
         document.addEventListener('DOMContentLoaded', function() {
-    // call $groupChat->messages every 2 seconds
-    setInterval(function() {
-        var xhr = new XMLHttpRequest();
-        
-        xhr.onreadystatechange = function() {
-            console.log('Ready state:', xhr.readyState);
-            
-            if (xhr.readyState === XMLHttpRequest.DONE) {
-                if (xhr.status === 200) {
-                    // handle success
-                    console.log('Response:', xhr.responseText);
-                    let messages = JSON.parse(xhr.responseText);
-                    let html = '';
-                    messages.forEach(function(message) {
-                        html += '<p>Sender: ' + (message.emitter ? message.emitter.name : 'Unknown') + '</p>';
-                        html += '<p>Date: ' + message.date + '</p>';
-                        html += '<p>' + message.content + '</p>';
-                    });
-                    document.querySelector('#chat').innerHTML = html;
-                } else {
-                    // handle error
-                    console.error('Error:', xhr.status, xhr.statusText);
-                }
-            }
-        };
-        
-        xhr.open('GET', '/group-chats/{{ $groupChat->group_id }}/messages', true);
-        xhr.send();
-    }, 2000);
-});
-
-
-
+            setInterval(function() {
+                var xhr = new XMLHttpRequest();
+                
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState === XMLHttpRequest.DONE) {
+                        if (xhr.status === 200) {
+                            let messages = JSON.parse(xhr.responseText);
+                            let html = '';
+                            messages.forEach(function(message) {
+                                if (message.emitter.id === {{ auth()->id() }}) {
+                                    html += '<div class="text-right">';
+                                    html += '<p><strong>' + message.emitter.name + '</strong></p>';
+                                    html += '<p>' + message.content + '</p>';
+                                    html += '<p><small>' + message.date + '</small></p>';
+                                    html += '</div>';
+                                } else {
+                                    html += '<div class="text-left">';
+                                    html += '<p><strong>' + message.emitter.name + '</strong></p>';
+                                    html += '<p>' + message.content + '</p>';
+                                    html += '<p><small>' + message.date + '</small></p>';
+                                    html += '</div>';
+                                }
+                            });
+                            document.querySelector('#chat').innerHTML = html;
+                        }
+                    }
+                };
+                
+                xhr.open('GET', '/group-chats/{{ $groupChat->group_id }}/messages', true);
+                xhr.send();
+            }, 2000);
+        });
     </script>
 @endsection
