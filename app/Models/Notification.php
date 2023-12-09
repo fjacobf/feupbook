12db7a9293f43bd2168c7517bbb88691375bee5b
fojacob <up202102359@up.pt>
2023-12-09 17:01:47 +0000

    created notification model

---
+++ app/Models/Notification.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Notification extends Model
+{
+    use HasFactory;
+
+    protected $table = 'notifications';
+
+    protected $primaryKey = 'notification_id';
+
+    protected $fillable = [
+        'date',
+        'notified_user',
+        'message',
+        'notification_type',
+        'comment_id',
+        'post_id',
+        'group_id',
+        'viewed',
+    ];
+
+    public $timestamps = false;
+
+    public function notif_user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'notified_user','user_id');
+    }
+
+}
