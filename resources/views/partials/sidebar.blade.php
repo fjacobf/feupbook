8d94a1089a565e45e6f7166a17feac28d30af8cf
Luis Miguel Lima Tavares <up202108662@fe.up.pt>
2023-12-13 02:04:38 +0000

    Remove unnecessary code from sidebar.blade.php

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -85,7 +85,6 @@
       </ul>
     </div>
 
-    
     <hr>
     <div class="dropdown">
       <a href="/home" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle py-3" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false" style="font-size: 1.25rem;">
@@ -93,92 +92,6 @@
         <strong>{{ $currentUser ? $currentUser->username : 'Guest' }}</strong>
       </a>
       <hr>
-      <ul class="nav nav-pills flex-column mb-auto">
-        <li class="nav-item w-100">
-          <a href="{{ route('home') }}" class="nav-link align-middle {{ request()->is('home') ? 'active' : 'link-dark' }} py-2" aria-current="page" style="font-size: 1.25rem;">
-            <i class="bi bi-house-fill"></i>
-            <p class="d-none d-sm-inline m-0 ms-3">Home</p>
-          </a>
-        </li>
-        <li>
-          <a href="{{url('/search')}}" class="nav-link {{ request()->is('search') ? 'active' : 'link-dark' }} py-2" style="font-size: 1.25rem;">          
-          <i class="bi bi-search"></i>
-          <p class="d-none d-sm-inline m-0 ms-3">Search</p>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="nav-link link-dark py-2" style="font-size: 1.25rem;">
-            <i class="bi bi-chat-dots-fill me-3"></i>
-            <p class="d-none d-sm-inline m-0">Messages</p>
-          </a>
-        </li>
-        <li>
-          <a href="{{route('post.bookmarks')}}" class="nav-link link-dark py-2" style="font-size: 1.25rem;">
-            <i id="bookmark-sidebar" class="bi bi-bookmark-fill me-3"></i>
-            <p class="d-none d-sm-inline m-0">Bookmarks</p>
-          </a>
-        </li>
-        <li>
-          @auth
-          <a href="{{ route('user.profile', ['id' => auth()->user()->user_id]) }}" class="nav-link {{ request()->routeIs('user.profile') && (request()->route()->parameter('id') == auth()->user()->user_id) ? 'active' : 'link-dark' }} py-2" style="font-size: 1.25rem;">
-            <i class="bi bi-person-circle me-3"></i>
-            <p class="d-none d-sm-inline m-0">Profile</p>
-          </a>
-          @endauth
-          @guest
-          <a href="{{ route('login') }}" class="nav-link link-dark py-2" style="font-size: 1.25rem;">
-            <i class="bi bi-person-circle me-3"></i>
-            <p class="d-none d-sm-inline m-0">Profile</p>
-          </a>
-          @endguest
-        </li>
-        <li>
-          <a href="#" class="nav-link link-dark py-2" style="font-size: 1.25rem;">
-            <i class="bi bi-gear-fill me-3"></i>
-            <p class="d-none d-sm-inline m-0">Settings</p>
-          </a>
-        </li>
-        <!-- Button to toggle additional items -->
-        <li class="nav-item">
-          <a href="#" class="nav-link link-dark py-2" style="font-size: 1.25rem;" data-bs-toggle="collapse" data-bs-target="#additionalItems" aria-expanded="false" aria-controls="additionalItems">
-              <i class="bi bi-question-circle me-3"></i>
-              <p class="d-none d-sm-inline m-0">Help</p>
-          </a>
-        </li>
-      </ul>
-      <!-- Additional items (collapsed by default) -->
-      <div class="collapse" id="additionalItems">
-        <ul class="nav nav-pills flex-column mb-auto">
-            <li>
-                <a href="{{route('contacts')}}" class="nav-link link-dark py-2" style="font-size: 1.25rem;">
-                    Contacts
-                </a>
-            </li>
-            <li>
-                <a href="{{route('help')}}" class="nav-link link-dark py-2" style="font-size: 1.25rem;">
-                    Help
-                </a>
-            </li>
-            <li>
-                <a href="{{route('faq')}}" class="nav-link link-dark py-2" style="font-size: 1.25rem;">
-                    FAQ
-                </a>
-            </li>
-            <li>
-                <a href="{{route('about')}}" class="nav-link link-dark py-2" style="font-size: 1.25rem;">
-                    About
-                </a>
-            </li>
-        </ul>
-      </div>
-
-      
-      <hr>
-      <div class="dropdown">
-        <a href="/home" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle py-3" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false" style="font-size: 1.25rem;">
-          <img src="https://github.com/mdo.png" alt="" width="48" height="48" class="rounded-circle me-3">
-          <strong class="d-none d-sm-inline">{{ $currentUser ? $currentUser->username : 'Guest' }}</strong>
-        </a>
         @if (Auth::check())
           <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
             <li><a class="dropdown-item" href="#">Add another account</a></li>
