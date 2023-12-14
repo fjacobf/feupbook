de897e282c68d7fc3b75d62a23248addb5bc0dba
MiguelM <up202108851@up.pt>
2023-12-14 10:27:24 +0000

    avatars on search page

--- resources/views/partials/search-results.blade.php
+++ resources/views/partials/search-results.blade.php
@@ -5,7 +5,10 @@
         @foreach($users as $user)
             <div class="card mb-4" style="max-width: 600px;">
                 <div class="card-body">
-                    <h5 class="card-title">{{ $user->name }}</h5>
+                    <div>
+                        <img src="{{ asset('images/profile_pics/' . $user->avatar)}}" alt="{{ $user->name }}" class="rounded-circle" width="50" style="border: 1px solid #000000">
+                        <h5 class="card-title">{{ $user->name }}</h5>
+                    </div>
                     <p class="card-text text-secondary"><span>@</span>{{ $user->username }}</span>
                     <p class="card-text">{{ $user->bio }}</p>
                     <a href="{{ route('user.profile', ['id' => $user->user_id]) }}" class="btn btn-primary">View Profile</a>
