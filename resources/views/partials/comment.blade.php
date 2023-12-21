1074e8ed855c1345473b1ab687b4d524243c6ee5
fojacob <up202102359@up.pt>
2023-12-21 03:50:36 +0000

    solved reply comment frontend

--- resources/views/partials/comment.blade.php
+++ resources/views/partials/comment.blade.php
@@ -40,7 +40,7 @@
                 </form>
                 <hr>
             </div>
-            <div style="gap:10px; font-size: 0.8rem;" class="d-flex align-items-center mb-2">
+            <div style="gap:10px; font-size: 0.8rem;" class="d-flex align-items-center mb-3">
                 <p class="m-0 comment-date">{{time_since($comment->created_at)}}</p>
                 <p id="comment-like-count-{{$comment->comment_id}}" class="m-0 comment-likes">{{$comment->likeCounts()}} likes</p>
                 <a style="cursor:pointer;"  class="link-underline-secondary" onclick="reply({{ $comment->comment_id }})">Reply</a>
@@ -50,14 +50,16 @@
 @endif
 
 @foreach ($comment->replies()->orderBy('created_at', 'desc')->get() as $reply)
-    <div style="margin: 0 0 0.5rem 3rem;" class="d-flex flex-column">
+    <div class="d-flex flex-column ms-5 mb-3">
         <div class="d-flex justify-content-between">
-            <div style="margin-left:10px; display:flex; align-items: center;">
-                <a class="text-black link-underline link-underline-opacity-0" href="{{ route('user.profile', ['id' => $reply->user->user_id]) }}">
-                    <img src="{{ asset('images/profile_pics/' . $reply->user->avatar) }}" class="rounded-circle" alt="User avatar" style="width: 20px; height: 20px;">
-                    <strong style="margin:0; font-size:1.1rem;" class="card-text">{{ $reply->user->username }}&nbsp</strong>
-                </a>
-                <p class="card-text">{{ $reply->content }}</p>
+            <div class="d-flex align-items-center">
+                <div class="card-text">
+                    <img src="{{ asset('images/profile_pics/' . $reply->user->avatar) }}" class="rounded-circle" alt="User avatar" style="width: 25px; height: 25px;">
+                    <a class="text-black link-underline link-underline-opacity-0" href="{{ route('user.profile', ['id' => $reply->user->user_id]) }}">
+                        <strong style="margin:0; font-size:1.1rem;" class="card-text">{{ $reply->user->username }}&nbsp</strong>
+                    </a>
+                    <span>{{ $comment->content }}</span>
+                </div>
             </div>
             <div class="buttons d-flex">
                 @can('delete', $reply)
